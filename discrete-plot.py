8bdf8fa76c01b1b21debb896fd5ada42f7d2073f
johncf <john.ch.fr@gmail.com>
2017-03-31 20:36:37 +0530

    discrete-plot

---
+++ discrete-plot.py
@@ -0,0 +1,70 @@
+#!/bin/python
+
+import csv
+from utils import *
+
+def main(cfails_file, obspop_file, outfile='/tmp/plot.svg'):
+    # read input files
+    fail_xs, fail_ys = read_csv(cfails_file)
+    obs_xs, obs_ys = read_csv(obspop_file)
+
+    fail_xs /= 24*365
+    obs_xs /= 24*365
+    bin_width = 1
+    x_lims = shortest_common_limits(fail_xs, obs_xs)
+
+    disk_hours_bins = binned_integral(obs_xs, obs_ys, bin_width, 10, x_lims)
+    fails_delta_bins = binned_non_decreasing_delta(fail_xs, fail_ys, bin_width, x_lims)
+    disk_hrs_xs, _, disk_hrs_ys = unzip(disk_hours_bins)
+    fdelta_xs, _, fdelta_ys = unzip(fails_delta_bins)
+    xs, fr_ys, _ = failure_rate((fail_xs, fail_ys), (obs_xs, obs_ys))
+
+    # plot result
+    import matplotlib.pyplot as plt
+    fig, (ax1, ax2, ax) = plt.subplots(3, 1)
+    fig.set_size_inches(8, 10)
+
+    ax1.set_ylabel("Number of failures")
+    #ax1.set_xlim(-2e3, 62e3)
+    ax1.set_ylim(0, 4800)
+    ax1.plot(fail_xs, fail_ys, color='#cc5511', label='Cumulative')
+    ax1.bar(fdelta_xs, fdelta_ys, width=1, align='edge', color='#cc880088', label='Yearly deltas')
+    for x, y in zip(fdelta_xs, fdelta_ys):
+        ax1.text(x + 0.5, y, np.round(y).astype(int), ha='center', va='bottom')
+    ax1.legend()
+
+    ax2.set_ylabel("Number observed")
+    ax2.set_yscale("log")
+    ax2.set_ylim(90, 1e5)
+    ax2.plot(obs_xs, obs_ys, label='Number of disks under observation')
+    ax2.bar(disk_hrs_xs, disk_hrs_ys, width=1, align='edge', color='#0044ff55', label='Number of disk-years observed')
+    for x, y in zip(disk_hrs_xs, disk_hrs_ys):
+        ax2.text(x + 0.5, np.exp(np.log(y) + 0.2), np.round(y).astype(int), ha='center', va='bottom')
+    ax2.legend()
+
+    ax.set_xlabel("Power-on years")
+    ax.set_ylabel("Normalized AFR (#/yr)")
+    ax.set_yscale("log")
+    ax.set_ylim(1e-3, 2)
+    ax.plot(xs, fr_ys, color='#ff1100')
+    fr_discrete_ys = fdelta_ys/disk_hrs_ys
+    ax.bar(disk_hrs_xs, fr_discrete_ys, width=1, align='edge', color='#ff440055')
+    for x, y in zip(disk_hrs_xs, fr_discrete_ys):
+        ax.text(x + 0.5, np.exp(np.log(y) + 0.2), np.round(y, 2).astype(str), ha='center', va='bottom')
+
+    fig.tight_layout()
+    fig.savefig(outfile, bbox_inches="tight")
+    print("Written failure rate plot to", outfile)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Compute failure rate distribution over '
+                        'a parameter (such as power-on hours), given the cumulative failures '
+                        'and observed population over it.')
+    parser.add_argument('cumufails', metavar='CUMUFAILS', help='cumulative failures (csv file)')
+    parser.add_argument('obspop', metavar='OBSPOP', help='observed population (csv file)')
+    parser.add_argument('-o', metavar='FILE', default='/tmp/plot.svg',
+                        help='output failure rate to %(metavar)s in the specified format')
+                             #'Use any file type supported by matplotlib.')
+    args = parser.parse_args()
+    main(args.cumufails, args.obspop, outfile=args.o)
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def read_csv(csvfile):
         for row in reader:
             xs.append(float(row[0]))
             ys.append(float(row[1]))
-    return xs, ys
+    return np.array(xs), np.array(ys)
 
 def write_csv(xs, ys, csvfile):
     with open(csvfile, 'w') as file_:
@@ -18,53 +18,59 @@ def write_csv(xs, ys, csvfile):
         for row in zip(xs, ys):
             writer.writerow(row)
 
-# calculates the integral of ys over xs for each bin
-def binned_integral(xs, ys, bin_width, samples_per_bin, x_lims=None):
-    x_min, x_max = x_lims if x_lims is not None else xs.min(), xs.max()
+def shortest_common_limits(xs1, xs2):
+    return max(xs1.min(), xs2.min()), min(xs1.max(), xs2.max())
+
+def unzip(zipped):
+    return tuple(np.array(a) for a in zip(*zipped))
+
+def _binned_meta(xs, ys, bin_width, x_lims):
+    x_min, x_max = x_lims if x_lims is not None else (xs.min(), xs.max())
 
     bin_start = np.floor(x_min / bin_width) * bin_width
     if np.abs(bin_start - x_min) > bin_width:
         bin_start = x_min
 
     y_itp = interp1d(xs, ys, kind="linear")
+    num_bins = int(np.ceil((x_max - bin_start) / bin_width))
+    return x_min, x_max, bin_start, y_itp, num_bins
+
+# calculates the integral of ys over xs for each bin
+def binned_integral(xs, ys, bin_width, samples_per_bin, x_lims=None):
+    x_min, x_max, bin_start, y_itp, num_bins = _binned_meta(xs, ys, bin_width, x_lims)
 
     bins = []
-    num_bins = int(np.ceil((x_max - bin_start) / bin_width))
     for i in range(num_bins):
         left = bin_start + bin_width*i
         right = bin_start + bin_width*(i+1)
         sample_halfwidth = (right - left) / samples_per_bin / 2
         xs_new = np.linspace(left + sample_halfwidth, right - sample_halfwidth, num=samples_per_bin)
-        xs_lims_mask = np.logical_and(np.less_equal(left, xs_new), np.less_equal(xs_new, right))
-        ys_new = y_itp(xs_new) * xs_lims_mask.astype(np.float32)
-        bins.push((left, right, np.sum(ys_new) / samples_per_bin))
+        xs_lims_mask = np.logical_and(np.less_equal(x_min, xs_new), np.less_equal(xs_new, x_max))
+        xs_lims_mask = xs_lims_mask.astype(np.float32)
+        xs_masked = (xs_new - bin_start - bin_width)*xs_lims_mask + bin_start + bin_width
+        #print(xs_lims_mask, '\n', xs_masked)
+        ys_new = y_itp((xs_masked)*xs_lims_mask + x_min) * xs_lims_mask
+        bins.append((left, right, np.sum(ys_new) / samples_per_bin))
     return bins
 
 # calculate y-deltas between the left and right of each bin in a non-decreasing function
 def binned_non_decreasing_delta(xs, ys, bin_width, x_lims=None):
-    x_min, x_max = x_lims if x_lims is not None else xs.min(), xs.max()
-
-    bin_start = np.floor(x_min / bin_width) * bin_width
-    if np.abs(bin_start - x_min) > bin_width:
-        bin_start = x_min
-
-    y_itp = interp1d(xs, ys, kind="linear")
+    x_min, x_max, bin_start, y_itp, num_bins = _binned_meta(xs, ys, bin_width, x_lims)
 
     bins = []
-    num_bins = int(np.ceil((x_max - bin_start) / bin_width))
     for i in range(num_bins):
         left = bin_start + bin_width*i
         right = bin_start + bin_width*(i+1)
         y_left = y_itp(max(left, x_min))
         y_right = y_itp(min(right, x_max))
-        bins.push((left, right, y_right - y_left))
+        bins.append((left, right, y_right - y_left))
     return bins
 
 def savgol_fit(xs, ys, samples, window_size, poly_order, x_lims=None, dydx=False):
     # linear interpolation function
     y_itp = interp1d(xs, ys, kind="linear")
 
-    x_min, x_max = x_lims if x_lims is not None else xs.min(), xs.max()
+    x_min, x_max = x_lims if x_lims is not None else (xs.min(), xs.max())
 
     # linearly spaced x values between x_min and x_max
     xs_new = np.linspace(x_min, x_max, num=samples)
@@ -80,10 +86,9 @@ def savgol_fit(xs, ys, samples, window_size, poly_order, x_lims=None, dydx=False
     return xs_new, ys_sg, dydx_sg
 
 def failure_rate(cfails, obspop, num_samples=2000, window_size=169, poly_order=3):
-    cfails_xs, cfails_ys = np.array(cfails[0]), np.array(cfails[1])
-    obspop_xs, obspop_ys = np.array(obspop[0]), np.array(obspop[1])
-    x_lims = (max(1., cfails_xs.min(), obspop_xs.min()),
-              min(cfails_xs.max(), obspop_xs.max()))
+    cfails_xs, cfails_ys = cfails
+    obspop_xs, obspop_ys = obspop
+    x_lims = shortest_common_limits(cfails_xs, obspop_xs)
 
     f_xs, f_ys, f_dydxs = savgol_fit(cfails_xs, cfails_ys, num_samples, window_size, poly_order, x_lims, dydx=True)
     o_xs, o_ys, _ = savgol_fit(obspop_xs, obspop_ys, num_samples, window_size, poly_order, x_lims, dydx=False)
