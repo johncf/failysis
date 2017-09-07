950348390fbe3eeec780e84de39b291e1bbcdf66
John <johncf@protonmail.com>
2017-09-07 09:59:53 +0000

    Update base.sql

--- sql/base.sql
+++ sql/base.sql
@@ -1,5 +1,3 @@
--- these queries were written for hive
-
 CREATE TABLE disk_logs
 ( datestamp DATE,
   serial_no VARCHAR(32),
@@ -8,48 +6,33 @@ CREATE TABLE disk_logs
   blocks_written BIGINT
 );
 
-CREATE TABLE disk_details
-( serial_no VARCHAR(32),
-  phy_size_blocks BIGINT,
-  phy_size_mb INT
-);
-
 CREATE TABLE disk_fails
 ( serial_no VARCHAR(32),
   datestamp DATE
 );
 
+-- import data from csv files
+COPY disk_logs FROM '/path/to/disk_logs.csv' WITH (FORMAT csv);
+COPY disk_fails FROM '/path/to/disk_fails.csv' WITH (FORMAT csv);
+
 CREATE VIEW disk_summary AS
-SELECT dd.serial_no,
-       ls.first_seen, ls.last_seen,
-       ls.min_power_hrs, ls.max_power_hrs,
-       ls.min_blocks_read / phy_size_blocks / phy_size_mb AS min_mbs_read,
-       ls.max_blocks_read / phy_size_blocks / phy_size_mb AS max_mbs_read,
-       ls.min_blocks_written / phy_size_blocks / phy_size_mb AS min_mbs_written,
-       ls.max_blocks_written / phy_size_blocks / phy_size_mb AS max_mbs_written
-FROM disk_details AS dd LEFT SEMI JOIN (SELECT serial_no,
-                                               MIN(dl.datestamp) AS first_seen,
-                                               MAX(dl.datestamp) AS last_seen,
-                                               MIN(dl.power_hrs) AS min_power_hrs,
-                                               MAX(dl.power_hrs) AS max_power_hrs,
-                                               MIN(dl.blocks_read) AS min_blocks_read,
-                                               MAX(dl.blocks_read) AS max_blocks_read,
-                                               MIN(dl.blocks_written) AS min_blocks_written,
-                                               MAX(dl.blocks_read) AS max_blocks_written
-                                        FROM disk_logs
-                                        GROUP BY serial_no) AS ls
-     ON dd.serial_no = df.serial_no
+SELECT serial_no,
+       MIN(dl.datestamp) AS first_seen,
+       MAX(dl.datestamp) AS last_seen,
+       MIN(dl.power_hrs) AS min_power_hrs,
+       MAX(dl.power_hrs) AS max_power_hrs
+FROM disk_logs AS dl
+GROUP BY serial_no
 
+-- cumulative failures over power_hrs -- C(t)
+SELECT power_hrs, SUM(disk_count) OVER (ORDER BY power_hrs)
+FROM (SELECT power_hrs, COUNT(dl.serial_no) AS disk_count
+      FROM disk_logs AS dl INNER JOIN disk_fails AS df
+           ON dl.serial_no = df.serial_no AND
+              dl.datestamp = df.datestamp -- under the assumption that a corresponding activity log exists for every failure log
+      GROUP BY power_hrs) AS cu
 
--- cumulative failures over power_hrs
-SELECT max_power_hrs, SUM(disk_count) OVER (ORDER BY max_power_hrs)
-FROM (SELECT max_power_hrs, COUNT(ds.serial_no) AS disk_count
-      FROM disk_summary AS ds LEFT SEMI JOIN disk_fails AS df
-           ON ds.serial_no = df.serial_no
-      GROUP BY max_power_hrs) AS cu
-
-
--- observed population over power_hrs
+-- observed population over power_hrs -- N(t)
 WITH cgs AS (SELECT power_hrs,
                     SUM(contrib_step) AS contrib_group
              FROM (SELECT 1 AS contrib_step,
@@ -63,8 +46,6 @@ WITH cgs AS (SELECT power_hrs,
 SELECT power_hrs,
        SUM(contrib_group) OVER (ORDER BY power_hrs) AS observed_pop
 FROM cgs
-
-
 
 -- sanity check: max_power_hrs vs last seen power_hrs
 SELECT ds.serial_no, ds.last_seen, dl.power_hrs AS last_power_hrs, ds.max_power_hrs -- COUNT(DISTINCT ds.serial_no)
@@ -80,26 +61,6 @@ FROM disk_logs
 GROUP BY serial_no
 HAVING MAX(power_hrs) - MIN(power_hrs) > 24*(7*const_n + 1)
 -- DELETE FROM disk_logs AS dl USING (/* above query*/) dx WHERE dl.serial_no = dx.serial_no
-
-
--- top 100 disks based on avg load over observed life
-SELECT serial_no,
-       del_power_hrs,
-       del_mbs_read / del_power_hrs / 3600 AS avg_rrate_mbps,
-       del_blocks_written / del_power_hrs / 3600 AS avg_wrate_mbps
-FROM (SELECT serial_no,
-             max_power_hrs - min_power_hrs AS del_power_hrs,
-             max_mbs_read - min_mbs_read AS del_mbs_read,
-             max_mbs_written - min_mbs_written AS del_blocks_written
-      FROM disk_summary) AS ddd
-WHERE del_power_hrs <> 0
-ORDER BY (avg_rrate_mbps + avg_wrate_mbps) DESC
-LIMIT 100
-
--- number of disks which faced more than 10 GB/hr load avg over observed life
-SELECT COUNT(serial_no) FROM disk_summary
-WHERE (max_mbs_read + max_mbs_written - min_mbs_read - min_mbs_written)/max_power_hrs/1024 > 10
-
 
 -- sanity check: number of failures for the same disk
 SELECT df.serial_no AS serial_no,
@@ -117,10 +78,16 @@ GROUP BY df.serial_no
 HAVING datediff(max_date_failed, min_date_failed) > 7
 
 -- sanity check: number of logs after a failure
-SELECT df.serial_no AS serial_no,
-       df.datestamp AS date_failed,
-       sum(cast(df.datestamp >= dl.datestamp AS INT)) AS log_count
-FROM disk_fails AS df LEFT JOIN disk_logs AS dl
+SELECT df.serial_no,
+       SUM(1) AS ct
+FROM disk_fails AS df INNER JOIN disk_logs AS dl
      ON dl.serial_no = df.serial_no
-GROUP BY df.serial_no, df.datestamp
-HAVING log_count = 0
+WHERE dl.datestamp > df.datestamp
+GROUP BY df.serial_no
+
+-- sanity check: failure logs that don't have corresponding activity logs
+SELECT serial_no
+FROM disk_fails WHERE serial_no NOT IN
+  ( SELECT df.serial_no
+    FROM disk_fails AS df INNER JOIN disk_logs AS dl
+         ON dl.serial_no = df.serial_no AND dl.datestamp = df.datestamp)
