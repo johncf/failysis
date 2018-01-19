b602c724649982a253af36bfff74f5a03b400466
johncf <johncf@protonmail.com>
2018-01-20 01:22:45 +0530

    dist-plot old updates

--- dist-plot.py
+++ dist-plot.py
@@ -4,7 +4,7 @@ from scipy.stats import weibull_min as weibull, gamma, lognorm
 import matplotlib.pyplot as plt
 import numpy as np
 
-def main(xmin=0, xmax=16, outfile='/tmp/plot.svg', scale='normal'):
+def main(xmin=0, xmax=12, outfile='/tmp/plot.svg', scale='normal'):
     dist_pars = [['weibull', 2.84, 8.56],
                  ['gamma', 4.78, 1.75],
                  ['lognorm', 0.528, np.exp(2.0835)]]
@@ -13,7 +13,8 @@ def main(xmin=0, xmax=16, outfile='/tmp/plot.svg', scale='normal'):
     fig.set_size_inches(5.3, 3)
 
     ax.set_xlabel("Power-on years")
-    ax.set_ylabel("PDF")
+    ax.set_ylabel("CDF")
+    ax.axhline(y=0.5, c='0.6', linewidth=0.5, zorder=0)
     if scale == 'log':
         ax.set_yscale("log")
     #    ax.set_ylim(2e-1, 100)
@@ -22,8 +23,9 @@ def main(xmin=0, xmax=16, outfile='/tmp/plot.svg', scale='normal'):
 
     xs = np.linspace(xmin, xmax, 2000)
     for dist, p1, p2 in dist_pars:
-        ys = gen_ys(dist, [p1, p2], xs)
+        ys, median = gen_ys(dist, [p1, p2], xs)
         ax.plot(xs, ys, label=dist)
+        ax.axvline(x=median, c='0.6', linewidth=0.5, zorder=0)
 
     ax.legend()
     fig.savefig(outfile, bbox_inches="tight")
@@ -33,14 +35,17 @@ def gen_ys(dist, params, xs):
     p1 = params[0]
     sc = params[1]
     if dist == 'weibull':
-        f = lambda x: weibull.pdf(x, p1, scale=sc)
+        f = lambda x: weibull.cdf(x, p1, scale=sc)
+        median = weibull.median(p1, scale=sc)
     elif dist == 'gamma':
-        f = lambda x: gamma.pdf(x, p1, scale=sc)
+        f = lambda x: gamma.cdf(x, p1, scale=sc)
+        median = gamma.median(p1, scale=sc)
     elif dist == 'lognorm':
-        f = lambda x: lognorm.pdf(x, p1, scale=sc)
+        f = lambda x: lognorm.cdf(x, p1, scale=sc)
+        median = lognorm.median(p1, scale=sc)
     else:
         raise ValueError("Invalid dist")
-    return np.vectorize(f)(xs)
+    return np.vectorize(f)(xs), median
 
 
 if __name__ == '__main__':
